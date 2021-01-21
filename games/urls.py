52fa5aa4ff1d9047ed1d6c43bae9537c090c9c3c
Your Name <you@example.com>
2021-01-21 12:22:03 -0500

    update for games completed working towards delete working, issue in models and associations

--- games/urls.py
+++ games/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('user/<username>/', views.profile, name='profile'),
+    path('<int:game_id>/', views.game_details, name='game_details'),
+    path('<int:pk>/update/', views.GameUpdate.as_view(), name='games_update'),
+    path('<int:pk>/delete/', views.GameDelete.as_view(), name='games_delete'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
@@ -18,37 +19,61 @@ import requests
 # Games
 def index(request):
     games = Game.objects.all()
-    return render(request, 'index.html', { 'games': games})
+    return render(request, 'index.html', {'games': games})
+
+
+def game_details(request, game_id):
+    game = Game.objects.get(id=game_id)
+    return render(request, 'games/show.html', {'game': game})
 
 
 def add_game(request):
     context = {}
 
     # create object of Form
-    
+
     form = GameForm(request.POST)
 
     if form.is_valid():
         form.save()
 
-    context['form']= form
+    context['form'] = form
     return render(request, "add_games.html", context)
+
+
+class GameUpdate(UpdateView):
+    model = Game
+    fields = ['title', 'platforms', 'description']
+
+
+    def form_valid(self, form):
+        print(self)
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/games')
+    
+
+
+class GameDelete(DeleteView):
+    model = Game
+    success_url = '/games'
 
 # USER
 
+
 # Add LoginForm to this line...
-from django.contrib.auth.forms import AuthenticationForm
 # ...and add the following line...
-from django.contrib.auth import authenticate, login, logout
 ...
+
+
 def login_view(request):
-     # if post, then authenticate (user submitted username and password)
+    # if post, then authenticate (user submitted username and password)
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             u = form.cleaned_data['username']
             p = form.cleaned_data['password']
-            user = authenticate(username = u, password = p)
+            user = authenticate(username=u, password=p)
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -57,24 +82,32 @@ def login_view(request):
                     print('The account has been disabled.')
             else:
                 print('The username and/or password is incorrect.')
-    else: # it was a get request so send the emtpy login form
+    else:  # it was a get request so send the emtpy login form
         form = AuthenticationForm()
         return render(request, 'login.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
+
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            print(user)
             login(request, user)
-            return redirect('/')
+            return HttpResponseRedirect('/user/' + str(user))
+        else:
+            print('not valid..........................................')
+            form = UserCreationForm()
+            return render(request, 'signup.html', {'form': form})
     else:
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form})
+
 
 def profile(request, username):
     user = User.objects.get(username=username)
