d8de69bc7780e4f87c81af3e6879eebb6fe47c83
Your Name <you@example.com>
2021-01-26 18:46:30 -0500

    rearranged import os in settings

--- game_tracker/settings.py
+++ game_tracker/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-import os
 import dj_database_url
+import os
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
